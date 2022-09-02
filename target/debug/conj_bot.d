fa4ced7f256f9b638c17300395669b948c8ecd4d
Matt Marsico <60232670+mattmars3@users.noreply.github.com>
2022-09-01 19:51:59 -0500

    Delete target directory

--- target/debug/conj_bot.d
+++
@@ -1,1 +0,0 @@
-/root/Tech/Rust/conj_bot/target/debug/conj_bot: /root/Tech/Rust/conj_bot/src/get_conj.rs /root/Tech/Rust/conj_bot/src/main.rs
--- target/debug/deps/conj_bot-71bbd6ce363298b9.d
+++
@@ -1,8 +0,0 @@
-/root/Tech/Rust/conj_bot/target/debug/deps/conj_bot-71bbd6ce363298b9.rmeta: src/main.rs src/cmd_line.rs src/get_conj.rs src/handle_json.rs
-
-/root/Tech/Rust/conj_bot/target/debug/deps/conj_bot-71bbd6ce363298b9.d: src/main.rs src/cmd_line.rs src/get_conj.rs src/handle_json.rs
-
-src/main.rs:
-src/cmd_line.rs:
-src/get_conj.rs:
-src/handle_json.rs:
